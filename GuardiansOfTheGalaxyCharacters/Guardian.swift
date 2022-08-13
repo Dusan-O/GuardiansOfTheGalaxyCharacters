9579d115af1ef7acf6f3f671f1745747de4d8e9f
Dusan-O <dusan.orescanin@gmail.com>
2022-08-13 02:02:56 +0200

    Add guardian.swift , datas.swift + assets

---
+++ GuardiansOfTheGalaxyCharacters/Guardian.swift
@@ -0,0 +1,14 @@
+//
+//  Guardian.swift
+//  GuardiansOfTheGalaxyCharacters
+//
+//  Created by Dusan Orescanin on 13/08/2022.
+//
+
+import Foundation
+
+struct Gardian {
+    var name: String
+    var desc: String
+    var imagePath: String
+}
